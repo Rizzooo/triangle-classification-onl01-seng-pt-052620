773b9405d0185881182564d5a984283a26ef64e3
Ronald Burrell Jr <ronburrell05@gmail.com>
2020-06-26 17:04:59 +0000

    Automatically backed up by Learn

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -2,9 +2,9 @@ class Triangle
  
   def initialize(side1, side2, side3)
     @sides = []
-    @side1 = side1
-    @side2 = side2
-    @side3 = side3
+    @sides << side1
+    @sides << side2
+    @sides << side3
   end
   
   def valid?
