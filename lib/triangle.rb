3a6114f3514be54e40b0f5d36ae83fff03c6ad18
Ronald Burrell Jr <ronburrell05@gmail.com>
2020-06-26 16:30:49 +0000

    Automatically backed up by Learn

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -2,7 +2,9 @@ class Triangle
   attr_accessor :length_1, :length_2, :length_3
   
   def initialize(length_1:, length_2:, length_3)
-    length_1
+    @side_1 = length_1
+    @side_2 = length_2
+    @side_3 = length_3
   end
   
   def kind 
