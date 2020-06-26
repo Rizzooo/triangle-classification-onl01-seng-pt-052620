d5fd32470c10f2f82c265472c1f9f8c179aaa004
Ronald Burrell Jr <ronburrell05@gmail.com>
2020-06-26 16:52:11 +0000

    Automatically backed up by Learn

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -15,15 +15,7 @@ class Triangle
   end
   
   def kind 
-    if @side2 && @side2 && @side3 >= 0
-      do
-      if @side1 == @side2 && @side3
-        :equilateral
-      elsif @side1 || @side2 == @side3
-        :isosceles
-      elsif @side1 !== @side2 !== @side3
-        :scalene
-      else 
+    if @sides != 0 && 
         
   end
 end
