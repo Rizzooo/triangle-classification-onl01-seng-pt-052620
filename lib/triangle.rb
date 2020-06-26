af5c2a6fc09f26346e9ab9a9df9749c0cc532a0d
Ronald Burrell Jr <ronburrell05@gmail.com>
2020-06-26 16:33:43 +0000

    Automatically backed up by Learn

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -2,7 +2,7 @@ class Triangle
   attr_accessor :length_1, :length_2, :length_3
   
   def initialize(sides)
-    sides.each { |key, value| self.send (("#{key}="), value)
+    sides.each { |key, value| self.send (("#{key}="), value)}
   end
   
   def kind 
